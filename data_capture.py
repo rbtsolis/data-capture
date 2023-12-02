8c52b2a7c07ed04f86800d41e8009cc6ca2ad37f
Roberth Solís <roberth.solis@gmail.com>
2023-12-01 23:36:43 -0600

    data capture typing added

--- data_capture.py
+++ data_capture.py
@@ -1,41 +1,58 @@
+from typing import List, Dict
+
 
 class DataCapture:
-  def __init__(self):
-    self.data: list = []
-    self.counter: dict = {
-      'less': 0,
-      'greater': 0,
-      'equal': 0
-    }
+    """Data Capture class"""
 
-  def add(self, number):
-    self.data.append(number)
-    if number in self.counter:
-      self.counter['equal'] += 1
-    elif number < self.data[0]:
-      self.counter['less'] += 1
-    else:
-      self.counter['greater'] += 1
+    def __init__(self) -> None:
+        self.data: List[int] = []
+        self.counter: Dict[str, int] = {
+            'less': 0,
+            'greater': 0,
+            'equal': 0
+        }
 
-  def build_stats(self):
-    class Stats:
-      def __init__(self, counter, data):
-        self.counter = counter
-        self.data = data
+    def add(self, number: int) -> None:
+        self.data.append(number)
+        if number in self.counter:
+            self.counter['equal'] += 1
+        elif number < self.data[0]:
+            self.counter['less'] += 1
+        else:
+            self.counter['greater'] += 1
 
-      def less(self, value):
-        if value in self.counter:
-          return self.counter['less']
-        return len([num for num in self.data if num < value])
+    def build_stats(self) -> 'Stats':
+        class Stats:
+            def __init__(self, counter: Dict[str, int], data: List[int]) -> None:
+                self.counter = counter
+                self.data = data
 
-      def greater(self, value):
-        if value in self.counter:
-          return self.counter['greater']
-        return len([num for num in self.data if num > value])
+            def less(self, value: int) -> int:
+                if value in self.counter:
+                    return self.counter['less']
+                return len([num for num in self.data if num < value])
 
-      def between(self, low, high):
-        less_than_low = self.less(low)
-        greater_than_high = self.greater(high)
-        return len(self.data) - less_than_low - greater_than_high
+            def greater(self, value: int) -> int:
+                if value in self.counter:
+                    return self.counter['greater']
+                return len([num for num in self.data if num > value])
 
-    return Stats(self.counter, self.data)
+            def between(self, low: int, high: int) -> int:
+                less_than_low = self.less(low)
+                greater_than_high = self.greater(high)
+                return len(self.data) - less_than_low - greater_than_high
+
+        return Stats(self.counter, self.data)
+
+capture = DataCapture()
+capture.add(3)
+capture.add(9)
+capture.add(3)
+capture.add(4)
+capture.add(6)
+stats = capture.build_stats()
+
+
+#print(stats.less(4))
+#print(stats.between(3, 6))
+#print(stats.greater(4))
